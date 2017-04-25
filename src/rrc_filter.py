e5188d6c38cd85831bb469b904909d7aba174dcf
CalilQ <calil_queiroz@hotmail.com>
2017-04-25 20:31:58 -0300

    Create class skeleton

---
+++ src/rrc_filter.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Root raised cosine filter, with upsampling and downsampling.
+
+Main methods:
+    tx_filter: upsamples and filters input symbols
+    rx_filter: filters and downsamples input signal
+
+Created on Tue Apr 25 20:06:30 2017
+
+@author: Calil
+"""
+
+class RRCFilter(object):
+    def __init__(self,N,alpha,Ts,Fs,up_factor):
+        """
+        Constructor method. Initializes attributes:
+            
+        N: int
+            Filter length
+        alpha: float
+            Roll off factor (Valid values are [0, 1])
+        Ts: float
+            Symbol time
+        Fs: float
+            Sampling rate in Hz
+        up_factor: int
+            Upsampling factor
+        
+        Parameters
+        __________
+        
+        N: int
+            Filter length
+        alpha: float
+            Roll off factor (Valid values are [0, 1])
+        Ts: float
+            Symbol time
+        Fs: float
+            Sampling rate in Hz
+        up_factor: int
+            Upsampling factor
+        """
+        pass
+    
+    def tx_filter(self,symbols):
+        """
+        Upsamples and filters input symbols.
+
+        Parameters
+        ----------
+        symbols: 1D complex array
+            Input symbols
+
+        Returns
+        ---------
+        signal: 1D complex array
+            Output signal
+        """
+        pass
+    
+    def rx_filter(self,signal):
+        """
+        Filters and downsamples signal.
+
+        Parameters
+        ----------
+        signal: 1D complex array
+            Input signal
+        
+        Returns
+        ---------
+        symbols: 1D complex array
+            Output symbols
+        """
+        pass
+    
+    def upsample(self,symbols):
+        pass
+    
+    def apply_filter(self,sig):
+        pass
+    
+    def downsample(self,sig):
+        pass
+    
+    def filter_response(self):
+        pass
+    
+    
