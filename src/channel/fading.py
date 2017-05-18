997081c8ed54fce6b6829201a88959b7a9410c70
Eduardo Gonçalves Sousa <eng.eduardo.gs@gmail.com>
2017-05-18 11:59:01 -0300

    New version of fading.py
    Adjust Jake's model's formula.

--- src/channel/fading.py
+++ src/channel/fading.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 10 19:06:50 2017
-
-@author: eduardo
-"""
-
 import numpy as np
-from channel import Channel
+
+from channel.channel import Channel
 
 
 class Fading(Channel):
+
     def __init__(self, doppler_freq, m, k):
         self.__doppler_freq = doppler_freq
         self.__m = m
         self.__k = k
+        self.__fading = 1
 
     def update_channel(self):
         pass
@@ -26,7 +21,7 @@ class Fading(Channel):
             tetha_nk = beta_n + 2*np.pi*(self.__k-1)/(self.__m+1)
             alpha = 0
             rayleigh = list(range(self.__m))
-            rayleigh[n] = 2*np.sqrt(2)*np.array(np.cos(beta_n)+np.sin(beta_n))*(np.cos(2*np.pi*self._doppler_freq*t+tetha_nk))+(1/np.sqrt(2))*np.array(np.cos(alpha)+(np.sin(alpha)))*(np.cos(2*np.pi*self.__doppler_freq*t))
+            rayleigh[n] = 2*np.sqrt(2)*((np.cos(beta_n)+np.sin(beta_n)*1j)*np.cos(2*np.pi*self.__doppler_freq*t+tetha_nk)+(1/np.sqrt(2))*(np.cos(alpha)+np.sin(alpha)*1j)*np.cos(2*np.pi*self.__doppler_freq*t))
 
         self.__fading = np.sum(rayleigh)
         pkt_rx = self.__fading*pkt_tx
